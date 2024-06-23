53fdb15cb63580933919b2e5c153c27923b5fc40
akcaHalit <halitakca070@gmail.com>
2024-06-23 20:55:22 +0300

    Data Binding is set to use and provided access for design.

--- app/src/main/java/com/example/kotlin_mvmm_beginner/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_mvmm_beginner/MainActivity.kt
@@ -2,11 +2,52 @@ package com.example.kotlin_mvmm_beginner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kotlin_mvmm_beginner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+
+        binding.resultText.text = "0"
+
+        binding.buttonAdd.setOnClickListener{
+            val number1Text = binding.number1Text.text.toString()
+            val number2Text = binding.number2Text.text.toString()
+            val number1 = number1Text.toInt()
+            val number2 = number2Text.toInt()
+            val result = number1 + number2
+            binding.resultText.text = result.toString()
+        }
+
+        binding.buttonSub.setOnClickListener{
+            val number1Text = binding.number1Text.text.toString()
+            val number2Text = binding.number2Text.text.toString()
+            val number1 = number1Text.toInt()
+            val number2 = number2Text.toInt()
+            val result = number1 - number2
+            binding.resultText.text = result.toString()
+        }
+
+        binding.buttonMult.setOnClickListener{
+            val number1Text = binding.number1Text.text.toString()
+            val number2Text = binding.number2Text.text.toString()
+            val number1 = number1Text.toInt()
+            val number2 = number2Text.toInt()
+            val result = number1 * number2
+            binding.resultText.text = result.toString()
+        }
+
+        binding.buttonDiv.setOnClickListener{
+            val number1Text = binding.number1Text.text.toString()
+            val number2Text = binding.number2Text.text.toString()
+            val number1 = number1Text.toInt()
+            val number2 = number2Text.toInt()
+            val result = number1 / number2
+            binding.resultText.text = result.toString()
+        }
     }
 }
